50bd7738f88edd8defc68f2299c5ce86928aa376
jfranco9202 <jeremy.franco41@yahoo.com>
2022-01-31 13:17:35 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //Reversing the word
+            var Reversed = "";
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                Reversed += word.ToLower()[i];
+            }
+
+            //comparing the original with the reversed
+            if(Reversed==word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,25 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void MyTest(string word, bool expected)
         {
+            //Arrange
+            var tester = new UnitTest1();
+            
+            //Act
+            bool actual = tester.WordSmith(word);
 
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        private bool WordSmith(string word)
+        {
+            throw new NotImplementedException();
         }
     }
 }
